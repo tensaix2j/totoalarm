7036548e53cc8bd1a3e443f1e4199c9ca3ead0b0
tensaix2j <tensaix2j@gmail.com>
2015-06-22 21:19:40 +0800

    new file:   totoalarm.rb

---
+++ totoalarm.rb
@@ -0,0 +1,80 @@
+# tensaix2j
+
+require 'open-uri'
+require 'nokogiri'
+require 'time'
+require 'net/smtp'
+require 'json'
+
+
+$config = {
+	"-threshold" => 4000000
+}
+
+
+#----------
+def send_email( smtpserver, smtpport , usetls , sender , sender_password  , receivers , subject , body , displayname)
+
+	receiverList = receivers.to_s.split(',')
+	receiverList_str = receiverList.map{ |s| "<#{s}>"}.join(',')
+
+	puts "<send_email> Sending mail To :#{receiverList_str} #{subject}"
+
+	begin
+	        message = [ "From: #{displayname} <#{sender}>", "To: #{receiverList_str}","Date:#{ Time.now.rfc2822}", "MIME-Version: 1.0", "Content-type: text/html" , "Subject: #{subject}\n" ,  body ]
+	        smtp = Net::SMTP.new( smtpserver , smtpport )
+	        smtp.enable_starttls if usetls == true
+	    smtp.start( 'localhost', sender , sender_password , :login ) { |s|
+	        s.send_message( message.join("\n"), sender , *receiverList )
+	        }
+
+	rescue Exception => ex
+	        puts "<send email error> #{ ex.to_s}"
+	end
+
+end
+
+
+#-----------
+def main( argv )
+
+	$config = $config.merge( Hash[*argv] )
+
+	url  = "http://www.singaporepools.com.sg/en/Documents/SPPL/homepagebodyen.html"
+	
+	rawhtml = open(url).read
+	page = Nokogiri::HTML( rawhtml )   
+	
+	prize 		= page.css(".lottery .prize").first.text.gsub("$","").gsub(",","").to_i
+	drawdate 	= Time.parse( page.css(".lottery .end-date").first.text )
+
+	p prize 
+
+	if prize >= $config["-threshold"].to_i 
+
+		email_config = JSON.parse( open("config.json").read )
+		
+		send_email( 
+			email_config["smtphost"] , 
+			email_config["smtpport"] , 
+			email_config["usetls"] , 
+			email_config["username"] , 
+			email_config["password"] , 
+			email_config["recipients"] , 
+			"TOTO ALERT: Prize of next draw is : $#{prize} " , 
+			"Buy some tickets now!",
+			email_config["displayname"]
+		)
+
+	else 
+		puts "Prize $#{prize} is less than threshold of $#{ $config["-threshold"].to_i }"
+	end
+
+end
+
+main ARGV
+
+
+
+
+
